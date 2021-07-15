2d3274264d0e7b62e99e2b50b5f3435a0a641b9b
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-07-15 18:57:00 +0500

    keyingisi
    fefw

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,33 @@
-
+import userImg from "../img/userImg.png";
 
 function Home() {
   return (
     <div className="home">
-        efwrfef
+        <div className="container-home">
+            <div className="header-home">
+              <div className="brand-home">
+                 <h1>Badiiyat</h1>
+              </div>
+              <div className="nav-home">
+                  <ul>
+                    <li>Bosh sahifa</li>
+                    <li>Nasr</li>
+                    <li>Nazm</li>
+                    <li>Maqolalar</li>
+                    <li>Forum</li>
+                  </ul>
+                  <div className="user-img">
+                      <img src={userImg}></img>
+                  </div>
+              </div>
+            </div>
+            <div className="banner-home">
+
+            </div>
+            <div className="main-home">
+
+            </div>
+        </div>
     </div>
   );
 }
