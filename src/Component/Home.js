7d697512a14ef6a95c2e2a530b0015425a93185e
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-07-17 12:05:22 +0500

    updated
    deyarli oz qoldi

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,6 +2,9 @@ import userImg from "../img/userImg.png";
 import bannerImg from "../img/bannerImg.png";
 import bannerBG from "../img/bannerBG.png";
 import starBG from "../img/starBG.svg";
+import starDot from "../img/starDot.svg";
+import ornament from "../img/ornament.svg";
+import {BiSearchAlt} from "react-icons/bi";
 
 function Home() {
   return (
@@ -28,16 +31,50 @@ function Home() {
                 <div className="banner-img">
                     <img className="bannerBG" src={bannerBG} alt="fghfhgj"></img>
                     <img className="bannerImg" src={bannerImg} alt="fghfhgj"></img>
-                    <div><p>temuriylar davri adabiyoti</p></div>
+                    <div className="div1"><p>temuriylar davri adabiyoti</p></div>
+                    <div className="slider-button">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div>
                     <img className="starBG" src={starBG} alt="fghfhgj"></img>
                     <img className="starBG1" src={starBG} alt="fghfhgj"></img>
                     <img className="starBG2" src={starBG} alt="fghfhgj"></img>
+                    <img className="starDot" src={starDot} alt="fghfhgj"></img>
+                    <img className="starDot1" src={starDot} alt="fghfhgj"></img>
+                    <img className="starDot2" src={starDot} alt="fghfhgj"></img>
+                </div>
+                <div className="search-div">
+                  <h2>qidirish</h2>
+                  <form>
+                    <input type="text" placeholder="Adiblar, kitoblar, audiolar, maqolalar..."></input>
+                    <button type="button"><BiSearchAlt /> Izlash</button>
+                  </form>
                 </div>
             </div>
             <div className="main-home">
-
+                <h2>Asosiy kategoriyalar</h2>
+                <ul>
+                  <li>Temuriylar davri </li>
+                  <li>Jadid adabiyoti </li>
+                  <li>Sovet davri </li>
+                  <li>Mustaqillik davri </li>
+                </ul>
+                <div className="katalog">
+                  <div className="katalog-item">
+                    <div className="item-img">
+                        <img  alt="dfbkj sjhdf" />
+                    </div>
+                    <div className="item-text">
+                     
+                    </div>
+                  </div>
+                </div>
             </div>
         </div>
+          <img className="ornament" src={ornament} alt="jnknjk"></img>
+        
     </div>
   );
 }
